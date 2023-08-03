8ca7eaf3a645cd195e74cae26aeb7b93bb7a009b
Ciekce <kantaja98@gmail.com>
2023-08-03 14:53:31 +0100

    lazily try parsing shredder fen

--- utils/src/txt_to_data.rs
+++ utils/src/txt_to_data.rs
@@ -31,7 +31,7 @@ pub fn run(options: Options) -> Result<()> {
             let (cp, wdl) = annotation.split_once(" | ")?;
 
             // try parsing initially as a standard fen, retry as shredder fen if that fails
-            let board = Board::from_fen(fen, false).unwrap_or(Board::from_fen(fen, true).ok().unwrap());
+            let board = Board::from_fen(fen, false).unwrap_or_else(|_| Board::from_fen(fen, true).ok().unwrap());
             let cp: f32 = cp.parse().ok()?;
             let wdl: f32 = wdl.parse().ok()?;
 
